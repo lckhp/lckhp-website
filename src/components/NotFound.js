9b731ce66240b91d4d0862ce70fe7355413e2fcf
Shadow <aarushtimalsina123@gmail.com>
2024-08-06 15:55:47 +0545

    404 not found page implemented.

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,14 @@
 import React from "react";
-import "./NotFound.css";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className="not-found">
+    <div style={{ textAlign: "center", padding: "50px" }}>
       <h1>404 - Page Not Found</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
+      <p>
+        <Link to="/">Go back to the homepage</Link>
+      </p>
     </div>
   );
 };
